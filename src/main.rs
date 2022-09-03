1919b30a31b6a360efa935fd72c9e2295db08a2a
Alex <AlexFasMod@mail.ru>
2022-09-03 18:56:52 +0500

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("Hello world");
+    let world = first_word(&s);
+
+    s.clear();
+}
+
+fn first_word(s: &String) -> &str{
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate(){
+        if item == b' '{
+            return &s[0..i];
+        }
+    }
+    &s[..]
 }
